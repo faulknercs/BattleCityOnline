be98c96b6406c552e0dc22231a787277e5efcf4d
Artem Stepanyuk <faulknercs@yandex.ru>
2012-07-10 13:40:19 +0200

    Created GUI subdir. Added GuiText class.

---
+++ GameClient/GUI/GuiText.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace BattleCity.GameClient.GUI
+{
+    /// <summary>
+    /// Supports text rendering using OpenTK
+    /// </summary>
+    class GuiText
+    {
+        public GuiText()
+        {
+
+        }
+
+        public String Value
+        {
+            get
+            {
+                return textValue;
+            }
+            set
+            {
+                if (value != textValue)
+                {
+                    textValue = value;
+                    // TODO: needs to recalculate text parameters
+                }
+            }
+        }
+
+        public void Render()
+        {
+            if (Value == null || Value == "")
+                return;
+        }
+
+        private Font textFont;
+        private String textValue;
+        private int textWidth;
+        private int textHeight;
+    }
+}
