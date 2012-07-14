d78a0054505a52f8b0b4a71c31f4a76155fd5f09
Ramboo <mikita_1991@mail.ru>
2012-07-15 00:52:40 +0300

    ( Mode + Map Generator ) into Server

--- DedicatedSrever/Main.cs
+++ DedicatedSrever/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BattleCity.GameLib;
 
 namespace BattleCity.DedicatedSrever
 {
@@ -11,9 +12,11 @@ namespace BattleCity.DedicatedSrever
             getCmd();
         }
 
-        private static string localName = "";
+        public static string localName = "";
+        public static GameMode mode = new GameMode(GameMode.Mode.CLASSIC);
+        public static Map map = new Map(MapGenerator.generateCLASSIC_Map());
 
-        private static void getCmd()
+        public static void getCmd()
         {
             while (true)
             {
@@ -44,10 +47,68 @@ namespace BattleCity.DedicatedSrever
                 case "hello":
                     Console.Write(Message.hello(localName));
                     break;
-                case "name":
+                case "set":
                     if (cmdParams.Length > 1 && (cmdParams[1] = cmdParams[1].Replace(" ", "")).Length > 0)
-                        localName = cmdParams[1];
-                    Console.Write(Message.hello(localName));
+                        switch (cmdParams[1].Replace(" ", "").ToLower())
+                        {
+                            case "name":
+                                if (cmdParams.Length > 2 && (cmdParams[2] = cmdParams[2].Replace(" ", "")).Length > 0)
+                                {
+                                    localName = cmdParams[2];
+                                    Console.Write(Message.hello(localName));
+                                }
+                                break;
+                            case "mode":
+                                switch (cmdParams[2].Replace(" ", "").ToLower())
+                                {
+                                    case "classic":
+                                        mode = new GameMode(GameMode.Mode.CLASSIC);
+                                        map = new Map(MapGenerator.generateCLASSIC_Map());
+                                        break;
+                                    case "dm":
+                                        mode = new GameMode(GameMode.Mode.DM);
+                                        map = new Map(MapGenerator.generateDM_Map());
+                                        break;
+                                    case "tdm":
+                                        mode = new GameMode(GameMode.Mode.TDM);
+                                        map = new Map(MapGenerator.generateTDM_Map());
+                                        break;
+                                    case "tdmb":
+                                        mode = new GameMode(GameMode.Mode.TDMB);
+                                        map = new Map(MapGenerator.generateTDMB_Map());
+                                        break;
+                                }
+                                Console.Write(Message.mode());
+                                break;
+                            case "map":
+                                Console.Write(Message.map());
+                                break;
+                            default:
+                                Console.Write(string.Format("{0}{1}",
+                                                            Message.error("No such cmd."),
+                                                            Message.help()));
+                                break;
+                        }
+                    break;
+                case "get":
+                    if (cmdParams.Length > 1 && (cmdParams[1] = cmdParams[1].Replace(" ", "")).Length > 0)
+                        switch (cmdParams[1].Replace(" ", "").ToLower())
+                        {
+                            case "name":
+                                Console.Write(Message.hello(localName));
+                                break;
+                            case "mode":
+                                Console.Write(!mode.Equals(null) ? Message.mode() : Message.error("Mode is not setted"));
+                                break;
+                            case "map":
+                                Console.Write(!map.Equals(null) ? Message.map() : Message.error("Set mode before"));
+                                break;
+                            default:
+                                Console.Write(string.Format("{0}{1}",
+                                                            Message.error("No such cmd."),
+                                                            Message.help()));
+                                break;
+                        }
                     break;
                 case "quit":
                     return "quit";
