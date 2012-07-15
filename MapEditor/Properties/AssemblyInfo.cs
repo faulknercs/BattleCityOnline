4e29db1b1d1a6e64442d68f4a0708d26d1801be0
Artem Stepanyuk <faulknercs@yandex.ru>
2012-07-15 15:18:44 +0200

    Added MapEditor project. Added OpenTK.GLControl.dll. Removed TestProject

--- MapEditor/Properties/AssemblyInfo.cs
+++ MapEditor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MapEditor")]
+[assembly: AssemblyDescription("Map Editor is editor to create and edit BattleCity maps.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestProject")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("MapEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2012 BattleCity Online team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a74c3f6b-f27a-4cc3-80c2-85214bcbe3b7")]
+[assembly: Guid("e9cb1575-8e06-4c2a-abb4-5627d2e50f98")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
