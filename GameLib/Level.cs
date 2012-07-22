e7002dba021c4817ac9879f62bc55770e20eaf45
Artem Stepanyuk <faulknercs@yandex.ru>
2012-07-22 16:17:54 +0200

    Started work on gamelib.

---
+++ GameLib/Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleCity.GameLib
+{
+    class Level : Map
+    {
+        public Level(Map map)
+            : base(map.GetInternalForm())
+        {
+
+        }
+
+        public IList<TankObject> GetTanks()
+        {
+            return tanks;
+        }
+
+        private IList<TankObject> tanks = new List<TankObject>();
+    }
+}
